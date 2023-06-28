0a4dab32cd0e5071435439526a4984da371a99aa
Cleslley-Viana <cleslley@outlook.com.br>
2023-06-27 23:47:48 -0300

    updated migrations

---
+++ ApiProducts/Configs/RegisterRepositories.cs
@@ -0,0 +1,12 @@
+﻿using ApiProducts.Repositories;
+
+namespace ApiProducts.Configs
+{
+    public static class RegisterRepositories
+    {
+        public static void RegisterMyRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IProductRepository, ProductRepository>();
+        }
+    }
+}
---
+++ ApiProducts/Configs/RegisterServices.cs
@@ -0,0 +1,13 @@
+﻿using ApiProducts.Services;
+
+namespace ApiProducts.Configs
+{
+    public static class RegisterServices
+    {
+        public static void RegisterMyServices(this IServiceCollection services)
+        {
+            services.AddScoped<IProductService, ProductService>();
+        }
+    }
+}
+
