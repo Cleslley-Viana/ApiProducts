460638562f2b66b1d3a2fb00d3b2ce0cb6dd6f40
Cleslley-Viana <cleslley@outlook.com.br>
2023-06-27 23:45:35 -0300

    initial project setup

--- ApiProducts/Program.cs
+++ ApiProducts/Program.cs
@@ -1,6 +1,15 @@
+using ApiProducts.Configs;
+using ApiProducts.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 26));
+builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(connectionString, serverVersion));
+builder.Services.RegisterMyServices();
+builder.Services.RegisterMyRepositories();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +17,14 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<AppDbContext>();
+    context.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
