6876f9ebf8a234bcc9dc76ace5c1d277f6dbbe3a
Cleslley-Viana <cleslley@outlook.com.br>
2023-06-27 23:46:29 -0300

    created db context

---
+++ ApiProducts/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ApiProducts.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiProducts.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        { 
+        
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
